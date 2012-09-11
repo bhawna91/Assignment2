c901c859460d7a5b237dfef940e1f1e5df1b9487
bhawna <bhawna.faujdar@gmail.com>
2012-09-11 20:06:09 +0530

    Adding files from Intellij

---
+++ tests/HelloWorldTest.java
@@ -0,0 +1,13 @@
+/**
+* Created with IntelliJ IDEA.
+* User: Bhawna
+* Date: 9/7/12
+* Time: 7:45 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class HelloWorldTest {
+    @org.junit.Test
+    public void testMain() throws Exception {
+
+    }
+}
